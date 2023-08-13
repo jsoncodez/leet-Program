70eb4f5537a7a77657cb0dd4f4a7973ee15da685
ohmni <jason20@umbc.edu>
2023-08-13 12:21:05 -0400

    Create main.cpp
    Example 1:
    
    Input: s = "the sky is blue"
    Output: "blue is sky the"
    Example 2:
    
    Input: s = "  hello world  "
    Output: "world hello"
    Explanation: Your reversed string should not contain leading or trailing spaces.
    Example 3:
    
    Input: s = "a good   example"
    Output: "example good a"
    Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.

---
+++ leet-reverseWordsInString/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <stack>
+
+
+using namespace std;
+
+class Solution {
+public:
+
+    void disp(string s) {
+
+        cout << endl << "s: " << s << endl;
+    }
+    string reverseWords(string s) {
+   
+        int spaceIdx1 = s.find_first_not_of(" ");
+   
+        stack<string> stackWord;
+
+        s = s.substr(spaceIdx1, s.find_last_not_of(" ") -spaceIdx1 +1);
+        
+  
+        int j = s.find(" ");
+        string tempWord;
+
+        
+        while (j != -1) {
+
+
+            tempWord = s.substr(0, j);
+
+            stackWord.push(tempWord);
+
+            s = s.substr(j);
+       
+
+            s = s.substr(s.find_first_not_of(" "));
+        
+
+            j = s.find(" ");
+            
+        }
+
+
+        while (!stackWord.empty()) {
+            cout << stackWord.top() << endl;
+            s += " " + stackWord.top();
+            stackWord.pop();
+        }
+     
+        cout << "s final =" << s << "..." << endl;
+        return s;
+    }
+};
+
+int main() {
+
+    Solution solve;
+
+    string s = " the sky is blue          ";
+    /* blue is sky the */
+    solve.reverseWords(s);
+    return 0;
+}
